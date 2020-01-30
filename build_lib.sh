2002e21b6e6c7038e27a3a2482720c965d984f75
Chris Jefferson <caj21@st-andrews.ac.uk>
2020-01-30 10:50:05 +0000

    Block warnings

--- build_lib.sh
+++ build_lib.sh
@@ -14,7 +14,7 @@ cat > ${mytmpdir}/source.cc
 
 GAC=${GAPPATH}/gac
 
-MYCFLAGS="-Wall -Wextra -g -I${MY_PATH}/gap_cpp_headers  -Wno-unused-parameter -Wno-missing-field-initializers -O"
+MYCFLAGS="-Wall -Wextra -g -I${MY_PATH}/gap_cpp_headers -Wno-pragmas -Wno-cast-function-type -Wno-unused-parameter -Wno-missing-field-initializers -O3 -march=native -mtune=native"
 
 BUILDSTUFF="-L ${CPPLIB} -o ${mytmpdir}/source.so -d ${mytmpdir}/source.cc"
 
